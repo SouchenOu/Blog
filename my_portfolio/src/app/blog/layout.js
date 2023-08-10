af3636273314e43a9c261e485dca62f2ca6617fc
Soukaina Ouchen <souchen@e1r10p1.1337.ma>
2023-08-10 14:18:11 +0100

    working on blog page

--- my_portfolio/src/app/blog/layout.js
+++ my_portfolio/src/app/blog/layout.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import styles from "./page.module.css"
 // children means all the children components that is exist in this folder bLOG
 
 const layout = ({children}) => {
   return (
     <div>
-        <h1>Hi this is Blog layout</h1>
+        <h1 className={styles.title}>Hi this is Blog layout</h1>
       {children} 
     </div>
   )
